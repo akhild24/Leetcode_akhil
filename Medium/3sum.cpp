29b03e7596bd8e571398a8753a21b9b4412c1843
akhild24 <akhildwivedi453@gmail.com>
2026-06-30 00:49:01 +0530

    minor chnage

--- Medium/3sum.cpp
+++ Medium/3sum.cpp
@@ -20,7 +20,7 @@ public:
                     l++;
                     r--;
                     while (l < r && nums[l] == nums[l-1]) { l++; }  // fix: && not 'and'
-                    while (l < r && nums[r] == nums[r+1]) { r--; }  // fix: && not 'and'
+                    while (r>=0 && nums[r] == nums[r+1]) { r--; }  // fix: && not 'and'
                 }
                 else if (s < sum) { l++; }
                 else { r--; }
