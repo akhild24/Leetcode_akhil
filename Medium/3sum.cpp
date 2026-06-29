6bba3ac956df802080ac07b584b927c6f09bc513
akhild24 <akhildwivedi453@gmail.com>
2026-06-30 00:33:47 +0530

    2 pointer 3 sum

---
+++ Medium/3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
+        vector<vector<int>> result;  // add this
+
+        for (int i = 0; i < n-2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) { continue; }  // fix: () and ;
+            
+            int l = i+1;      // fix: declare int
+            int r = n-1;      // fix: declare int
+            int sum = -nums[i];  // fix: nums[i] not nums[1]
+
+            while (l < r) {
+                int s = nums[l] + nums[r];
+                if (s == sum) {
+                    result.push_back({nums[i], nums[l], nums[r]});  // fill this
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l-1]) { l++; }  // fix: && not 'and'
+                    while (l < r && nums[r] == nums[r+1]) { r--; }  // fix: && not 'and'
+                }
+                else if (s < sum) { l++; }
+                else { r--; }
+            }
+        }
+
+        return result;
+    }
+};
